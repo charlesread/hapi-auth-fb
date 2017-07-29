608b0cb76b72a03291dfcdc5177b6c7e7409a927
Charles Read <physixrox@gmail.com>
2017-07-29 18:29:31 -0400

    initial

---
+++ example/1 - simple.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const Hapi = require('hapi')
+
+const plugins = [
+  {
+    register: require('../index'),
+    options: {
+      client_id: '',
+      client_secret: '',
+      success: function (credentials) {
+        console.log(credentials)
+      }
+    }
+  }
+]
+
+const server = new Hapi.Server()
+
+server.connection({
+  host: 'localhost',
+  port: 8000
+})
+
+server.register(plugins, function (err) {
+  if (err) {
+    throw err
+  }
+  server.auth.strategy('facebook', 'facebook')
+
+  // an insecure route
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+      return reply('Welcome to the app! Check out <a href="/secure">/secure</a> to see a secured endpoint.')
+    }
+  })
+
+  // a secure route, will redirect to FB for auth
+  server.route({
+    method: 'GET',
+    path: '/secure',
+    config: {
+      auth: 'facebook'
+    },
+    handler: function (request, reply) {
+      const credentials = request.auth.credentials
+      return reply(`Hi, ${credentials.first_name}!  Your email address is ${credentials.email} (according to Facebook).`)
+    }
+  })
+
+  server.start((err) => {
+
+    if (err) {
+      throw err
+    }
+    console.log('Server running at:', server.info.uri)
+  })
+})
---
+++ index.js
@@ -0,0 +1,90 @@
+'use strict'
+
+const path = require('path')
+const debug = require('debug')('hapi-auth-fb:plugin')
+const co = require('bluebird-co').co
+
+const _options = require(path.join(__dirname, 'lib', 'options.js'))
+const utility = require(path.join(__dirname, 'lib', 'utility.js'))
+
+let pluginOptions
+
+const internals = {}
+
+const plugin = function (server, options, next) {
+  pluginOptions = _options(server, options)
+  debug('plugin registered')
+  debug('pluginOptions: %j', pluginOptions)
+  server.ext('onRequest', function (request, reply) {
+    debug('received request for %s [%s]', request.path, request.method)
+    reply.continue()
+  })
+  server.auth.scheme('facebook', internals.scheme)
+  server.register({
+    register: require('yar'),
+    options: pluginOptions.yar
+  }, function (err) {
+    if (err) {
+      throw err
+    }
+    server.route({
+      method: 'get',
+      path: pluginOptions.handlerPath,
+      handler: function (request, reply) {
+        const destination = request.yar.get('destination')
+        debug('code: %s', request.query.code)
+        debug('destination: %s', destination)
+        co(function *() {
+          const appAccessToken = yield utility.getAppAccessToken()
+          debug('appAccessToken: %s', appAccessToken)
+          const userAccessToken = yield utility.getUserAccessToken(request.query.code)
+          debug('userAccessToken: %s', userAccessToken)
+          const userAccessTokenInfo = yield utility.debugUserAccessToken(userAccessToken, appAccessToken)
+          debug('userAccessTokenInfo:')
+          debug(userAccessTokenInfo)
+          const user_id = userAccessTokenInfo.data.user_id
+          const userInfo = yield utility.getUserInfo(userAccessToken, user_id)
+          debug('userInfo:')
+          debug(userInfo)
+          request.yar.set(pluginOptions.credentialsName, userInfo)
+          reply.redirect(destination)
+        })
+          .catch((err) => {
+            console.error(err.message)
+          })
+      }
+    })
+    next()
+  })
+}
+
+plugin.attributes = {
+  pkg: require('./package.json')
+}
+
+internals.scheme = function () {
+  const _scheme = {}
+  _scheme.authenticate = function (request, reply) {
+    debug('_scheme.authenticate called')
+    if (!request.yar.get('destination')) {
+      debug('destination is not set, setting to request.path')
+      request.yar.set('destination', request.path)
+    }
+    debug('destination: %s', request.yar.get('destination'))
+    const credentials = request.yar.get(pluginOptions.credentialsName)
+    if (credentials) {
+      if (pluginOptions.success && typeof pluginOptions.success === 'function') {
+        pluginOptions.success(credentials)
+      }
+      debug('credentials does exist')
+      reply.continue({credentials})
+    } else {
+      debug('credentials does not exist, redirecting to FB for auth')
+      reply(null, null, {})
+        .redirect(pluginOptions.fb.dialogUrl)
+    }
+  }
+  return _scheme
+}
+
+module.exports = plugin
